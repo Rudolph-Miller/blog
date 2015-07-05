83192045ec979f77095253631442ed3553dcee23
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2015-07-05 22:18:00 +0900

    mv public/* ./.

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,10 @@ git clean -f
 git fetch origin gh-pages
 git reset --hard FETCH_HEAD
 git checkout gh-pages
-git rebase origin master
+git merge origin/master --ff --no-edit
 hugo
-cd public
-git add . -f
+mv public/* ./
+git add .
 git commit -m "Update $(date +%s). [ci skip]"
 git push origin gh-pages
 git checkout master
